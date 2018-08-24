8afc1e2251bc91ff20a6d3a23e2e77cd2f0af8b4
hdelgadillo <33609841+hdelgadillo@users.noreply.github.com>
2018-08-24 02:14:32 -0500

    f

---
+++ practica2/cor.vhd
@@ -0,0 +1,10 @@
+library ieee;
+use ieee.std_logic_1164.all;
+
+entity cor is
+port(a,b: in std_logic; c: out std_logic);
+end entity cor;
+architecture arqcor of cor is
+begin 
+c<= a or b;
+end architecture arqcor;
